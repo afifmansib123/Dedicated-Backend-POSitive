2ba959758f6023f70bfd6d6d3988c20798279bd6
Afif-Mac <afifmansib123@gmail.com>
2024-09-02 16:28:32 +0700

    it is i think

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.set("strictQuery", false);
-    await mongoose.connect("mongodb+srv://afifmansib123:Ageekis0cool!@cluster0.5laae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://afifmansib123:Ageekis0cool!@cluster0.zhnoq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
   } catch (error) {
     throw error;
   }
